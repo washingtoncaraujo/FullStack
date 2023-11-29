6fd2b485b255f911bb0699ba8a083a52e8d614df
washingtoncaraujo <111454028+washingtoncaraujo@users.noreply.github.com>
2023-11-29 20:26:36 -0300

    url render

--- ControledeViagens_V2/src/config.js
+++ ControledeViagens_V2/src/config.js
@@ -1,7 +1,8 @@
 // Arquivo de configuração para a URL da API
 
 // Define a URL da API que será usada no projeto. Certifique-se de substituir esta URL pela URL real da sua API.
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://clientebd.onrender.com'
+//'http://localhost:8080';
 //"https://650250ed736d26322f5ced53.mockapi.io/api";
 
 // Exporta a URL da API como padrão, para que ela possa ser importada em outros arquivos.
